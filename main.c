e1f4861e6d055f0113c29e20b999f3f692d5d7d2
Mohammed Hisham <theghost1712@gmail.com>
2018-11-09 17:19:47 +0200

    first version of output_wave

--- main.c
+++ main.c
@@ -6,43 +6,76 @@
 #include "playback.h"
 #include "wave.h"
 
+
+typedef enum _shape{
+	SINE = 0,SQUARE,SAWTOOTH,TRIANGULAR
+}Shape;
+
+typedef struct _wave{
+	Shape shape;
+	unsigned int freq;
+	unsigned long amp;
+}Wave;
+
 static int err;
-int main(int argc,char **argv){
 
-	char *data1 = NULL, *data2 = NULL;
-	char *ptr = NULL, *buffer = NULL;
+const char* wave_names[] = {"Sine","Square","Sawtooth","Triangular"};
 
-	err = init();
-	check_err("init");
+int output_waves(Wave *ch1_wave,Wave *ch2_wave){
 
-	err = print_params();
-	check_err("print_params");
+	/*These two pointers point to the memory allocated by generate_<wave> functions
+	 * These allocated chunks of memory stores the data to be produced using function play
+	 */
+	char *ch1_samples = NULL, *ch2_samples = NULL;
+	/*Holds the returned pointer from function play*/
+	char *ring_buffer = NULL;
+	/*Checks if calls to functions generate_<wave> was successfull or not*/
+	char *rc_ptr = NULL;
 
-	while(1){
-		ptr = generate_square(1000,30000,&data1);
-		check(ptr,"generate_square:1");
-
-		ptr = generate_saw(1000,30000,&data2);
-		check(ptr,"generate_sine:2");
-
-		buffer = play(data1,data2);
-		check(buffer,"play");
+	switch(ch1_wave->shape){
+		case SINE:
+			rc_ptr = generate_sine(ch1_wave->freq,ch1_wave->amp,&ch1_samples);
+			break;
+		case SQUARE:
+			rc_ptr = generate_square(ch1_wave->freq,ch1_wave->amp,&ch1_samples);
+			break;
+		case SAWTOOTH:
+			rc_ptr = generate_saw(ch1_wave->freq,ch1_wave->amp,&ch1_samples);
+			break;
+		case TRIANGULAR:
+			rc_ptr = generate_triangular(ch1_wave->freq,ch1_wave->amp,&ch1_samples);
+			break;
+		default:
+			sentinel("This value(%d) doesn't represent a waveform",ch1_wave->shape);
 	}
+	check(rc_ptr,"rc_ptr in NULL");
 
+	//Do same thing for channel 2
+	switch(ch2_wave->shape){
+		case SINE:
+			rc_ptr = generate_sine(ch2_wave->freq,ch2_wave->amp,&ch2_samples);
+			break;
+		case SQUARE:
+			rc_ptr = generate_square(ch2_wave->freq,ch2_wave->amp,&ch2_samples);
+			break;
+		case SAWTOOTH:
+			rc_ptr = generate_saw(ch2_wave->freq,ch2_wave->amp,&ch2_samples);
+			break;
+		case TRIANGULAR:
+			rc_ptr = generate_triangular(ch2_wave->freq,ch2_wave->amp,&ch2_samples);
+			break;
+		default:
+			sentinel("This value(%d) doesn't represent a waveform",ch2_wave->shape);
+	}
+	check(rc_ptr,"rc_ptr is NULL");
 	return 0;
 error:
-	if(data1){
-		free(data1);
-	}
-	if(data2){
-		free(data2);
-	}
-	if(buffer){
-		free(buffer);
-	}
-	if(hwparams){
-		free(hwparams);
-	}
+	return -1;
+}
+
+int main(int argc,char **argv){
+
 	return 1;
 }
+
 
