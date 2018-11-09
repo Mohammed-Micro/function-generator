2bfa508c575e855ef3b8bf112440a8851e06b4de
Mohammed Hisham <theghost1712@gmail.com>
2018-11-09 18:05:48 +0200

    done with output_waves function

--- main.c
+++ main.c
@@ -28,26 +28,26 @@ int output_waves(Wave *ch1_wave,Wave *ch2_wave){
 	 */
 	char *ch1_samples = NULL, *ch2_samples = NULL;
 	/*Holds the returned pointer from function play*/
-	char *ring_buffer = NULL;
+	char *buffer = NULL;
 	/*Checks if calls to functions generate_<wave> was successfull or not*/
 	char *rc_ptr = NULL;
 
 	//Define type pointer to function that returns char* and takes (unsgined long,unsigned long,char **)
 	typedef char *(*generate_ptr)(unsigned long,unsigned long,char **);
 	//Array of pointers to functions of type generate_ptr
-	generate_ptr generates[] = {&generate_sine,&generate_square,&generate_sawtooth,&generate_triangular};
+	generate_ptr generates[] = {&generate_sine,&generate_square,&generate_saw,&generate_triangular};
 	//Define pointers that can be used to call the appropriate generate_<wave> function for each channel
 	generate_ptr ch1_generate_wave_ptr = generates[ch1_wave->shape];
 	generate_ptr ch2_generate_wave_ptr = generates[ch2_wave->shape];
 
 	while(1){
 		rc_ptr = (*ch1_generate_wave_ptr)(ch1_wave->freq,ch1_wave->amp,&ch1_samples);
-		check(rc_ptr,"rc_ptr is NULL");
+		check(rc_ptr,"generate_%s",wave_names[ch1_wave->shape]);
 		rc_ptr = (*ch2_generate_wave_ptr)(ch2_wave->freq,ch2_wave->amp,&ch2_samples);
-		check(rc_ptr,"rc_ptr is NULL");
+		check(rc_ptr,"generate_%s",wave_names[ch2_wave->shape]);
 
 		buffer = play(ch1_samples,ch2_samples);
-		check(buffer,"buffer is NULL");
+		check(buffer,"play");
 	}
 	return 0;
 error:
@@ -55,7 +55,7 @@ error:
 		free(ch1_samples);
 	if(ch2_samples)
 		free(ch2_samples);
-	if(buffer){
+	if(buffer)
 		free(buffer);
 
 	return -1;
@@ -63,7 +63,15 @@ error:
 
 int main(int argc,char **argv){
 
+
+	Wave ch1 = {SINE,1000,30000};
+	Wave ch2 = {SINE,1000,-30000};
+	init();
+	err = output_waves(&ch1,&ch2);
+	return 0;
+error:
 	return 1;
+
 }
 
 
