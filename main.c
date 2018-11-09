8316de4c187c702b356e4688f7197317ae86299c
Mohammed Hisham <theghost1712@gmail.com>
2018-11-09 18:19:47 +0200

    Modifying main.c to be usable with wave.c and wave.h in master branch

--- main.c
+++ main.c
@@ -19,7 +19,7 @@ typedef struct _wave{
 
 static int err;
 
-const char* wave_names[] = {"Sine","Square","Sawtooth","Triangular"};
+const char* wave_names[] = {"sine","square","saw","triang"};
 
 int output_waves(Wave *ch1_wave,Wave *ch2_wave){
 
@@ -35,12 +35,13 @@ int output_waves(Wave *ch1_wave,Wave *ch2_wave){
 	//Define type pointer to function that returns char* and takes (unsgined long,unsigned long,char **)
 	typedef char *(*generate_ptr)(unsigned long,unsigned long,char **);
 	//Array of pointers to functions of type generate_ptr
-	generate_ptr generates[] = {&generate_sine,&generate_square,&generate_saw,&generate_triangular};
+	generate_ptr generates[] = {&generate_sine,&generate_square,&generate_saw,&generate_triang};
 	//Define pointers that can be used to call the appropriate generate_<wave> function for each channel
 	generate_ptr ch1_generate_wave_ptr = generates[ch1_wave->shape];
 	generate_ptr ch2_generate_wave_ptr = generates[ch2_wave->shape];
 
 	while(1){
+		//Call generate_<wave> functions using pointers to them
 		rc_ptr = (*ch1_generate_wave_ptr)(ch1_wave->freq,ch1_wave->amp,&ch1_samples);
 		check(rc_ptr,"generate_%s",wave_names[ch1_wave->shape]);
 		rc_ptr = (*ch2_generate_wave_ptr)(ch2_wave->freq,ch2_wave->amp,&ch2_samples);
@@ -66,10 +67,17 @@ int main(int argc,char **argv){
 
 	Wave ch1 = {SINE,1000,30000};
 	Wave ch2 = {SINE,1000,-30000};
-	init();
+
+	err = init();
+	check_err("init");
+
 	err = output_waves(&ch1,&ch2);
+	check_err("output_waves");
+
 	return 0;
 error:
+	if(hwparams)
+		free(hwparams);
 	return 1;
 
 }
